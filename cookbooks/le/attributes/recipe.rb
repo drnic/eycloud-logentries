d7f9dcf022bd75b8f1bea012e31ccbf496aebd44
Dr Nic Williams <drnicwilliams@gmail.com>
2012-01-09 22:18:57 -0800

    move the attribute implementation into normal recipe

--- cookbooks/le/attributes/recipe.rb
+++ cookbooks/le/attributes/recipe.rb
@@ -1,11 +1,4 @@
 le_version('0.8.15')
 le_user_key('YOUR_ACCOUNT_KEY_GOES_HERE')
-follow_paths = [
-  "/var/log/syslog",
-  "/var/log/auth.log"
-]
-(node[:applications] || []).each do |app_name, app_info|
-  follow_paths << "/var/log/nginx/#{app_name}.access.log"
-end
-le_follow(follow_paths)
 # TODO - get this from /data/*/current/
+
