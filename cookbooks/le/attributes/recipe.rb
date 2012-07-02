2eb2bece2a654d7da565295cef371e09dfbd255f
Caroline Fenlon <carfenlon@gmail.com>
2012-07-02 17:25:12 +0200

    Update master

--- cookbooks/le/attributes/recipe.rb
+++ cookbooks/le/attributes/recipe.rb
@@ -1,2 +1,6 @@
-le_user_key('YOUR_ACCOUNT_KEY_GOES_HERE')
+le_user_key('YOUR_ACCOUNT_KEY_GOES_HERE')  #FIXME - change the constant for your account key
+# Read more at https://logentries.com/doc/engineyard/
+
+
+
 # TODO - get this from /data/*/current/
