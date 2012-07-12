6b4434f038b332a4c6b313a361b0b535037e2c96
Caroline Fenlon <carfenlon@gmail.com>
2012-07-12 15:00:05 +0200

    Do not fail if already registered/following file

--- cookbooks/le/recipes/configure.rb
+++ cookbooks/le/recipes/configure.rb
@@ -6,11 +6,13 @@
 execute "le register --user-key" do
   command "le register --user-key #{node[:le_user_key]}"
   action :run
+  not_if { File.exists?('/etc/le/config') } 
 end
 
 follow_paths = [
   "/var/log/syslog",
-  "/var/log/auth.log"
+  "/var/log/auth.log",
+  "/var/log/daemon.log"
 ]
 (node[:applications] || []).each do |app_name, app_info|
   follow_paths << "/var/log/nginx/#{app_name}.access.log"
@@ -19,6 +21,7 @@ end
 follow_paths.each do |path|
   execute "le follow #{path}" do
     command "le follow #{path}"
+    ignore_failure true 
     action :run
   end
 end
