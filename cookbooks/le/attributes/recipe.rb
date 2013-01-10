fdda6da118355defe231f087719fe30eed6ce373
Mark Lacomber <marklacomber@gmail.com>
2013-01-10 20:17:55 +0000

    Removed comment's from attributes/recipe.rb as it's live now

--- cookbooks/le/attributes/recipe.rb
+++ cookbooks/le/attributes/recipe.rb
@@ -1,6 +1,2 @@
-le_api_key('YOUR_API_KEY_GOES_HERE')  #FIXME - change the constant for your account key
+le_api_key('YOUR_API_KEY_GOES_HERE')  
 # Read more at https://logentries.com/doc/engineyard/
-
-
-
-# TODO - get this from /data/*/current/
